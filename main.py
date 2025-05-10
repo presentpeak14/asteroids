682a3b26066d44ab78f36947516a3d1c3a7f1234
presentpeak14 <eliot718@gmail.com>
2025-05-10 09:18:06 -0400

    The player can now rotate properly

--- main.py
+++ main.py
@@ -17,10 +17,11 @@ def main():
 
         screen.fill("black")
         player.draw(screen)
+        dt = clock.tick(60) / 1000        # limit the framerate to 60 FPS
+        player.update(dt)
         pygame.display.flip()
 
-        # limit the framerate to 60 FPS
-        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
