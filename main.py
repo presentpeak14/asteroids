24c15717d4082d93f5caa784108c1e7dbb23e46a
presentpeak14 <eliot718@gmail.com>
2025-05-10 10:04:24 -0400

    I added colision between asteroids and the player

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 from constants import *
 from player import Player
-
+from asteroids import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -11,8 +12,20 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()  # Plural form as per assignment
+
+    # Set containers for Player
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    # Set containers for Asteroid
+    Asteroid.containers = (asteroids, updatable, drawable)  # Needs to be a tuple with all three groups
+
+    # Set containers for AsteroidField (only updatable)
+    AsteroidField.containers = (updatable,)  # Note the comma to make this a tuple
+
+    # Create the asteroid field
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
@@ -24,6 +37,10 @@ def main():
             entity.draw(screen)
         dt = clock.tick(60) / 1000        # limit the framerate to 60 FPS
         updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
         pygame.display.flip()
 
 
